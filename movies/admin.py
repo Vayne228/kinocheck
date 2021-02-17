28c9ae33e5b3efbaae010ca45f3b0ab9f2903e1a
Vayne228 <prutkin95@mail.ru>
2021-02-17 14:38:17 +0300

    add actions in admin panel

--- movies/admin.py
+++ movies/admin.py
@@ -48,6 +48,7 @@ class MovieAdmin(admin.ModelAdmin):
 	save_on_top = True
 	save_as = True
 	list_editable = ("draft",)
+	actions = ["publish", "unpublish"]
 	form = MovieAdminForm
 	readonly_fields = ("get_image",)
 	fieldsets = (
@@ -74,6 +75,28 @@ class MovieAdmin(admin.ModelAdmin):
 
 	def get_image(self, obj):
 		return mark_safe(f'<img src={obj.poster.url} width="100" height="110"')
+
+	def unpublish(self, request, queryset):
+		row_update = queryset.update(draft=True)
+		if row_update == 1:
+			message_bit = '1 note was updated'
+		else:
+			message_bit = f"{row_update} notes were updated"
+		self.message_user(request, f"{message_bit}")
+
+	def publish(self, request, queryset):
+		row_update = queryset.update(draft=False)
+		if row_update == 1:
+			message_bit = '1 note was updated'
+		else:
+			message_bit = f"{row_update} notes were updated"
+		self.message_user(request, f"{message_bit}")
+
+	publish.short_description = "Publish"
+	publish.allowed_permission = ('change',)
+
+	unpublish.short_description = "Unpublish"
+	unpublish.allowed_permission = ('change',)
 
 	get_image.short_description = "Image"
 
